fc7f86f3ef0bec1aabdd4489b656b09f4c7a220d
kencx <kennethcheo@pm.me>
2023-01-09 18:40:10 +0800

    format update_catalog.py

--- update_catalog.py
+++ update_catalog.py
@@ -5,35 +5,36 @@ import datetime
 import hashlib
 import json
 import os
-from pathlib import Path
 import shutil
 import sys
+from pathlib import Path
+
 
 def read_json_file(filename):
     try:
-        with open(filename, 'r') as f:
+        with open(filename, "r") as f:
             data = json.load(f)
             return data
     except OSError as err:
         sys.exit(f"FATAL: Cannot open file {filename}: {err}")
 
+
 def check(filename, checksum, checksum_type):
     BUF_SIZE = 65536
-    d = {"sha256": hashlib.sha256(),
-         "sha1": hashlib.sha1(),
-         "md5": hashlib.md5()}
+    d = {"sha256": hashlib.sha256(), "sha1": hashlib.sha1(), "md5": hashlib.md5()}
 
     h = d[checksum_type]
     if not h:
         raise ValueError(f"Checksum type {checksum_type} not supported")
 
-    with open(filename, 'rb') as f:
+    with open(filename, "rb") as f:
         while True:
             data = f.read(BUF_SIZE)
             if not data:
                 break
             h.update(data)
     return checksum == h.hexdigest()
+
 
 def main():
     if not os.path.isfile(args.box):
@@ -49,7 +50,7 @@ def main():
     if args.checksum.startswith("file://"):
         checksum_path = Path(args.checksum.split("file://")[1])
         try:
-            with open(checksum_path, 'r') as f:
+            with open(checksum_path, "r") as f:
                 s = f.read()
                 checksum = s.split()[0]
         except OSError as err:
@@ -67,9 +68,9 @@ def main():
                 "name": args.provider,
                 "url": "file://" + args.box,
                 "checksum_type": args.checksum_type,
-                "checksum": checksum
+                "checksum": checksum,
             }
-        ]
+        ],
     }
     metadata["versions"].append(data)
 
@@ -79,52 +80,55 @@ def main():
         backup = f"{args.catalog}_{now:%y%m%d-%H%M%S}"
         shutil.copy(args.catalog, backup)
     except OSError as err:
-        sys.exit(f"FATAL: Cannot create backup of Vagrant catalog metadata file \
+        sys.exit(
+            f"FATAL: Cannot create backup of Vagrant catalog metadata file \
             {args.catalog}: {err}"
         )
 
     try:
-        with open(args.catalog, 'w') as f:
+        with open(args.catalog, "w") as f:
             json.dump(metadata, f, indent=2)
     except OSError as err:
-        sys.exit(f"FATAL: Cannot write to Vagrant catalog metadata file \
+        sys.exit(
+            f"FATAL: Cannot write to Vagrant catalog metadata file \
             {args.catalog}: {err}"
         )
 
     print("INFO: Wrote new metadata successfully!")
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Update Vagrant catalog metadata file"
-    )
-    parser.add_argument("-f", "--catalog",
+    parser = argparse.ArgumentParser(description="Update Vagrant catalog metadata file")
+    parser.add_argument(
+        "-f",
+        "--catalog",
         required=True,
         type=str,
-        help="Full path to catalog metadata file"
+        help="Full path to catalog metadata file",
     )
-    parser.add_argument("-v", "--version",
-        type=str,
-        help="New box version"
-    )
-    parser.add_argument("-p", "--provider",
+    parser.add_argument("-v", "--version", type=str, help="New box version")
+    parser.add_argument(
+        "-p",
+        "--provider",
         default="libvirt",
         type=str,
         choices=["libvirt", "virtualbox"],
-        help="Box provider"
+        help="Box provider",
     )
-    parser.add_argument("-b", "--box",
-        type=str,
-        help="Path to Vagrant box file"
-    )
-    parser.add_argument("-t", "--checksum_type",
+    parser.add_argument("-b", "--box", type=str, help="Path to Vagrant box file")
+    parser.add_argument(
+        "-t",
+        "--checksum_type",
         default="sha256",
         type=str,
         choices=["sha256", "sha1", "md5"],
-        help="Checksum type"
+        help="Checksum type",
     )
-    parser.add_argument("-c", "--checksum",
+    parser.add_argument(
+        "-c",
+        "--checksum",
         type=str,
-        help="Checksum of box file. Can be string or 'file:///path/to/checksum/file'"
+        help="Checksum of box file. Can be string or 'file:///path/to/checksum/file'",
     )
     args = parser.parse_args()
 
